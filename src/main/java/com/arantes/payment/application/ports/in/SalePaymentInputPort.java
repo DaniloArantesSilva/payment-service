e9d8dc9bf589dd26a62facdcb412902fb8fd36ba
Danilo Arantes <daniloarantes_00@hotmail.com>
2023-02-24 15:09:19 -0300

    First commit

---
+++ src/main/java/com/arantes/payment/application/ports/in/SalePaymentInputPort.java
@@ -0,0 +1,9 @@
+package com.arantes.payment.application.ports.in;
+
+import com.arantes.payment.application.core.domain.Sale;
+
+public interface SalePaymentInputPort {
+
+    void payment(Sale sale);
+
+}
